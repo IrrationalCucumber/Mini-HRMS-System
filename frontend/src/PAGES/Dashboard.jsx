0ace9cb73e48bcdc7994347bd90e9eb64ceec2aa
adrean paul sorono <adreanpaulsorono@gmail.com>
2026-06-19 15:05:37 +0800

    Initial display counter in dashboard

--- frontend/src/PAGES/Dashboard.jsx
+++ frontend/src/PAGES/Dashboard.jsx
@@ -7,12 +7,59 @@ Employees on Leave
 Total Monthly Payroll
 
 */
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Dashboard = () => {
+  const apiUrl = process.env.REACT_APP_API_URL 
+  const [displayCounts, setDisplayCounts] = useState({
+    totalEmployees: 0,
+    activeEmployees: 0,
+    employeesOnLeave: 0,
+    totalMonthlyPayroll: 0
+  });
+  //get employee counts from backend
+  useEffect(() => {
+    const fetchDashboardData = async () => {
+      try {
+        const resTotal = await axios.get(`${apiUrl}/employee/total`);
+        const resActive = await axios.get(`${apiUrl}/employee/active`);
+        const resOnleave = await axios.get(`${apiUrl}/employee/onleave`);
+        setDisplayCounts({
+          totalEmployees: resTotal.data.totalEmployees,
+          activeEmployees: resActive.data.count,
+          employeesOnLeave: resOnleave.data.count,
+        });
+      } catch (error) {
+        console.error('Error fetching dashboard data:', error);
+      }
+    };
+
+    fetchDashboardData();
+  }, [apiUrl]);
+  //fetch total mpayroll from backend
+  useEffect(() => {
+    const fetchTotalPayroll = async () => {
+      try {
+        const resPayroll = await axios.get(`${apiUrl}/payroll/all`);
+        console.log(resPayroll);
+      } catch (error) {
+        console.error('Error fetching total payroll:', error);
+      }
+    };
+
+    fetchTotalPayroll();
+  }, [apiUrl]);
+
   return (
     <>
       <h2>Dashboard</h2>
       <p>Welcome to the Dashboard!</p>
+      <div>
+        <h3>Total Employees: {displayCounts.totalEmployees}</h3>
+        <h3>Active Employees: {displayCounts.activeEmployees}</h3>
+        <h3>Employees on Leave: {displayCounts.employeesOnLeave}</h3>
+      </div>
     </>
   );
 };
