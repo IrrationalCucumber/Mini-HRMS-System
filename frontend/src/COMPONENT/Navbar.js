d3ef1f485a70e6672a19c14b0c7d2a581eb0fb39
adrean paul sorono <adreanpaulsorono@gmail.com>
2026-06-22 19:41:23 +0800

    Navbar UI

--- frontend/src/COMPONENT/Navbar.js
+++ frontend/src/COMPONENT/Navbar.js
@@ -1,26 +1,43 @@
 import { Link } from "react-router-dom";
+import "./style.css";
+import { Typography } from "@mui/joy";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <div className="nav-container">
-                <h2>Mini-HRMS</h2>
-                <ul className="nav-menu">
-                    <li className="nav-item">
-                       <Link to={"/dashboard"}>Dashboard</Link>
-                    </li>
-                    <li className="nav-item">
-                       <Link to={"/employees"}>Employees</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to={"/attendance"} className="nav-link">
-                            Attendance
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="nav-container">
+        <Typography level="h2">Mini-HRMS</Typography>
+
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <Typography>
+              <Link to={"/dashboard"}>Dashboard</Link>{" "}
+            </Typography>
+          </li>
+          <li className="nav-item">
+            <Typography>
+              <Link to={"/employees"}>Employees Management</Link>{" "}
+            </Typography>
+          </li>
+          <li className="nav-item">
+            <Typography>
+              <Link to={"/attendance"} className="nav-link">
+                Attendance
+              </Link>{" "}
+            </Typography>
+          </li>
+          <li className="nav-item">
+            <Typography>
+              {" "}
+              <Link to={"/salary"} className="nav-link">
+                Salary Management
+              </Link>
+            </Typography>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
