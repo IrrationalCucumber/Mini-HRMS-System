b0de79fe080ed8661194857e11fa8f1e1bef52aa
adrean paul sorono <adreanpaulsorono@gmail.com>
2026-06-23 16:59:15 +0800

    Add api route for generating payroll

--- backend/ROUTE/PayrollRoute.js
+++ backend/ROUTE/PayrollRoute.js
@@ -6,5 +6,5 @@ router.get("/all", payrollController.getAllPayroll);
 router.get("/employee/:id", payrollController.getEmpPayroll);
 router.post("/add", payrollController.addEmpPayroll);
 router.put("/update/:id", payrollController.updateEmpPayroll);
-
+router.post("/generate", payrollController.generatePayroll);
 module.exports = router;
